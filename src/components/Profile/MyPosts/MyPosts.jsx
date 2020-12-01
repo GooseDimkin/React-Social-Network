1e77cbefdb163ad646234e787dfe704e1aff3a60
GooseDimkin <lebedevdmitry7660882@gmail.com>
2020-12-01 14:57:50 +0200

    Bugfix
    Added prototype of likes and fixed bugs

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,11 +7,11 @@ function MyPosts() {
         <div className={s.posts}>
             My posts
             <hr />
-            <Post text='Pls stop spaming!' id='owner'/>
-            <Post text='Allahu Akbar' />
-            <Post text='Lol Kek Cheburek'/>
-            <Post text='I like ya cut G'/>
-            <Post text='Alabama'/>
+            <Post text='Pls stop spaming!' likes='228' id='owner'/>
+            <Post text='Allahu Akbar' likes='0'/>
+            <Post text='Lol Kek Cheburek' likes='11' />
+            <Post text='I like ya cut G'likes='2' />
+            <Post text='Alabama' likes='0' />
         </div>
     );
 }
--- src/components/Profile/MyPosts/Posts/Post.jsx
+++ src/components/Profile/MyPosts/Posts/Post.jsx
@@ -1,13 +1,14 @@
 import s from './Post.module.css';
 
-import Profile from 'D:/nodejs-first-project/first-app/src/components/Profile/Profile'
-
-let avatar__ = 'https://sun9-13.userapi.com/impg/3GV5l5eh2HxWoEDlApIa1TbqNxUfDbms91zFCQ/mR3fIh8Y3Y4.jpg?size=440x440&quality=96&proxy=1&sign=42dea185827e1e591f44a3cf211225e6';
+let avatar__;
 
 function Post(props) {
     if(props.id !== 'owner')
     {
         avatar__ = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png';
+    }
+    else {
+        avatar__ = 'https://sun9-13.userapi.com/impg/3GV5l5eh2HxWoEDlApIa1TbqNxUfDbms91zFCQ/mR3fIh8Y3Y4.jpg?size=440x440&quality=96&proxy=1&sign=42dea185827e1e591f44a3cf211225e6';
     }
 
     return(
@@ -15,7 +16,7 @@ function Post(props) {
             <img src={avatar__} />
             <div className={s.text}>{props.text}</div>
             <div>
-                <span>like</span>
+                <span>likes</span> {props.likes}
             </div>
         </div>
     );
